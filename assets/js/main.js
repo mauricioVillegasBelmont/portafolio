801ee437bcf5d2c6463b1ecf4102cdb2e23b8fcf
mauricioVillegasBelmont <mauricio.vi.be@gmail.com>
2018-07-27 01:04:34 -0500

    loading cover
    implementation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,65 @@
+$(window).on("load",function(){
+    loaded();
+});
 var visionEn= [['Have no fear of perfection—you’ll never reach it','Salvador Dali'], ['Digital design is like painting, except the paint never dries','Neville Brody'],['Creativity is a highfalutin word for the work I have to do between now and Tuesday','Ray Kroc'],['Creativity is nothing but the way to solve new problems.','Anonimo'],['People ignore designs that ignore people','Frank Chimero'],['For every complex problem, there is an answer that is clear, simple, and wrong','H. L. Mencken'], ['Design is not just what it looks like and feels like. Design is how it works','Steve Jobs'],['Those who don’t build must burn.','Ray Bradbury'],['If you think good design is expensive, you should look at the cost of bad design','Ralf Speth']];
 var vision= [['No tengas miedo a la perfección... — nunca la alcanzarás','Salvador Dali'], ['El diseño digital es como pintar, excepto que la pintura nunca se seca','Neville Brody'],['La creatividad es una palabra arrogante para el trabajo que tengo que hacer entre hoy y el martes','Ray Kroc'],['La creatividad no es más que la forma de resolver nuevos problemas','Anonimo'],['La gente ignora los diseños que ignoran a las personas','Frank Chimero'],['Para cada problema complejo, hay una respuesta que es clara, simple e incorrecta','H. L. Mencken'], ['El diseño no es solo lo que parece y se siente. El diseño es como funciona','Steve Jobs'],['Aquellos que no construyen deberian arder','Ray Bradbury'],['Si crees que un buen diseño es costoso, debes considerar el costo de un mal diseño','Ralf Speth']];
-
 var slider;
+var h = window.innerWidth * 4;
 
-$(document).ready(function(){
-    
-    //cover size adjustment
-    var h = window.innerWidth * 4;
-    $('#coverPage').css('height', h);
-    
-    //check the window scroll & setup
-    if($(window).scrollTop() != 0){
-       scrollEvents($(window).scrollTop());
-    }
-    
-    //contact behivior
-    $('#contact-close').on('click',function(){
-        $('footer').toggleClass('active');
-    });
-    
-    //phrases
-    var p = Math.floor(Math.random() * vision.length) + 0 ;
-    $('#vision blockquote').html(vision[p][0]);
-    $('#vision').append('<p>'+vision[p][1]+'</p>');
-    
-    //scroll transition
-    $("#nav a").click( function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 2000, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
-    
-    $(".scroll-next").click(function() {
-        event.preventDefault();
-		var cls = $(this).closest("section").next();
-        
-		$("html, body").animate({
-            scrollTop: cls.offset().top
-        }, 2000, function(){
-            window.location.hash = cls.attr('id');
-        });
-	});
-    
-    $('.item:eq(0)').addClass('active');
-    $('.dot:eq(0)').addClass('active');
-    portaNavButton(0);
-    
-    $('section').on('scrollSpy:enter', function() {
-        $('a[href = "#'+$(this).attr('id')+'" ]').addClass('active');
-    });
-    $('section').on('scrollSpy:exit', function() {
-        $('a[href = "#'+$(this).attr('id')+'" ]').removeClass('active');
-    });
-    $('section').scrollSpy();
-    
+
+
+//cover size adjustment
+$('#coverPage').css('height', h);
+
+if($(window).scrollTop() != 0){
+   scrollEvents($(window).scrollTop());
+}
+
+//contact behivior
+$('#contact-close').on('click',function(){
+    $('footer').toggleClass('active');
 });
+
+//phrases
+var p = Math.floor(Math.random() * vision.length) + 0 ;
+$('#vision blockquote').html(vision[p][0]);
+$('#vision').append('<p>'+vision[p][1]+'</p>');
+
+//scroll transition
+$("#nav a").click( function(event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 2000, function(){
+            window.location.hash = hash;
+        });
+    }
+});
+
+$(".scroll-next").click(function() {
+    event.preventDefault();
+    var cls = $(this).closest("section").next();
+
+    $("html, body").animate({
+        scrollTop: cls.offset().top
+    }, 2000, function(){
+        window.location.hash = cls.attr('id');
+    });
+});
+
+$('.item:eq(0)').addClass('active');
+$('.dot:eq(0)').addClass('active');
+portaNavButton(0);
+
+$('section').on('scrollSpy:enter', function() {
+    $('a[href = "#'+$(this).attr('id')+'" ]').addClass('active');
+});
+$('section').on('scrollSpy:exit', function() {
+    $('a[href = "#'+$(this).attr('id')+'" ]').removeClass('active');
+});
+$('section').scrollSpy();
 
 $( window ).resize(function(){
     console.log('window resize');
@@ -163,4 +162,10 @@ function closeModal(){
 function stopVideos(){
     webPV.stopVideo();
     mmedPV.stopVideo();
+}
+function loaded(){
+    $('body').addClass('loaded');
+    setTimeout(function(){
+        $( ".loading" ).remove();
+    }, 1500);
 }
